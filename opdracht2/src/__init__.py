b2d2226c0021dc3b14c0158f2ee6a8b798fcda61
Daan Hollands <daan.hollands@student.uhasselt.be>
2025-11-03 19:01:13 +0100

    ziet er wel goed uit I guess

---
+++ opdracht2/src/__init__.py
@@ -0,0 +1,4 @@
+from .feauture_detection import process
+
+# __module__.py
+__all__ = ["process"]
---
+++ opdracht2/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+from src import process
+
+
+def main():
+    process("resources/edm_image.png", False, video_path="resources/IMG_4011.mp4")
+
+
+if __name__ == "__main__":
+    main()
